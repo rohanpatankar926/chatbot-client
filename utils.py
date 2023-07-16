edfd0b67546494818ea4082409752a2624a28739
Rohan Patankar <57869463+rohanpatankar926@users.noreply.github.com>
2023-07-16 14:30:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,3 +10,19 @@ def read_yaml(config_path):
 
 config_loader = read_yaml(f"config.yaml")
 
+
+from pptx import Presentation
+import os
+import uuid
+import shutil
+def ppt_to_pdf(input_file, output_file):
+    presentation = Presentation(input_file)
+    presentation.export(output_file)
+
+if os.path.exists("ppt_to_pdf_dirs"):
+    shutil.rmtree("ppt_to_pdf_dirs")
+pdf_data_file=f"{str(uuid.uuid1())[:4]}.pdf"
+os.makedirs("ppt_to_pdf_dirs")
+input_file = "samplepptx.pptx"
+output_file =f"ppt_to_pdf_dirs/{pdf_data_file}"
+ppt_to_pdf(input_file, output_file)
